dd58e9337371456544b8d5ca02ab6dc7f4dcf641
Anshu Kumari <anshu.kumari@thoughtworks.com>
2023-05-25 21:27:35 +0530

    Add. Repository annotation

--- src/main/java/com/demo/employeemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/demo/employeemanagement/repository/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.demo.employeemanagement.repository;
 
 import com.demo.employeemanagement.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
     Optional<EmployeeEntity> findByAadhaar(Long id);
 
